3eb95ade0435477c422bca1daf72ad0dd1b1d0cf
zjqzlyzjq <2193542487@qq.com>
2017-12-13 15:16:38 +0800

    aa

--- js/index.js
+++ js/index.js
@@ -192,77 +192,76 @@
 // $(document).on('touchstart touchmove', function (e) {
 //     e.preventDefault();
 // });
-// let cubeRender = (function ($) {
-//     let $cubeBox = $('.cubeBox'),
-//         $box = $cubeBox.find('.box');
-//
-//     let touchBegin = function (e) {
-//         //=>this:box
-//         let point = e.changedTouches[0];
-//         $(this).attr({
-//             strX: point.clientX,
-//             strY: point.clientY,
-//             isMove: false,
-//             changeX: 0,
-//             changeY: 0
-//         });
-//     };
-//
-//     let touching = function (e) {
-//         let point = e.changedTouches[0],
-//             $this = $(this);
-//         let changeX = point.clientX - parseFloat($this.attr('strX')),
-//             changeY = point.clientY - parseFloat($this.attr('strY'));
-//         if (Math.abs(changeX) > 10 || Math.abs(changeY) > 10) {
-//             $this.attr({
-//                 isMove: true,
-//                 changeX: changeX,
-//                 changeY: changeY
-//             });
-//         }
-//     };
-//
-//     let touchEnd = function (e) {
-//         let point = e.changedTouches[0],
-//             $this = $(this);
-//         let isMove = $this.attr('isMove'),
-//             changeX = parseFloat($this.attr('changeX')),
-//             changeY = parseFloat($this.attr('changeY')),
-//             rotateX = parseFloat($this.attr('rotateX')),
-//             rotateY = parseFloat($this.attr('rotateY'));
-//         if (isMove === 'false') return;
-//
-//         rotateX = rotateX - changeY / 3;
-//         rotateY = rotateY + changeX / 3;
-//         $this.css(`transform`, `scale(.6) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`).attr({
-//             rotateX: rotateX,
-//             rotateY: rotateY
-//         });
-//     };
-//
-//     return {
-//         init: function () {
-//             $cubeBox.css('display', 'block');
-//
-//             //=>事件绑定实现相关效果
-//             $box.attr({
-//                 rotateX: -30,
-//                 rotateY: 45
-//             }).on({
-//                 touchstart: touchBegin,
-//                 touchmove: touching,
-//                 touchend: touchEnd
-//             });
-//
-//             //=>每一个页面的点击操作
-//             $box.find('li').tap(function () {
-//                 $cubeBox.css('display', 'none');
-//                 let index = $(this).index();
-//                 detailRender.init(index);
-//             });
-//         }
-//     }
-// })(Zepto);
+let cubeRender = (function ($) {
+    let $cubeBox = $('.cubeBox'),
+        $box = $cubeBox.find('.box');
+
+    let touchBegin = function (e) {
+        //=>this:box
+        let point = e.changedTouches[0];
+        $(this).attr({
+            strX: point.clientX,
+            strY: point.clientY,
+            isMove: false,
+            changeX: 0,
+            changeY: 0
+        });
+    };
+
+    let touching = function (e) {
+        let point = e.changedTouches[0],
+            $this = $(this);
+        let changeX = point.clientX - parseFloat($this.attr('strX')),
+            changeY = point.clientY - parseFloat($this.attr('strY'));
+        if (Math.abs(changeX) > 10 || Math.abs(changeY) > 10) {
+            $this.attr({
+                isMove: true,
+                changeX: changeX,
+                changeY: changeY
+            });
+        }
+    };
+
+    let touchEnd = function (e) {
+        let point = e.changedTouches[0],
+            $this = $(this);
+        let isMove = $this.attr('isMove'),
+            changeX = parseFloat($this.attr('changeX')),
+            changeY = parseFloat($this.attr('changeY')),
+            rotateX = parseFloat($this.attr('rotateX')),
+            rotateY = parseFloat($this.attr('rotateY'));
+        if (isMove === 'false') return;
+
+        rotateX = rotateX - changeY / 3;
+        rotateY = rotateY + changeX / 3;
+        $this.css(`transform`, `scale(.6) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`).attr({
+            rotateX: rotateX,
+            rotateY: rotateY
+        });
+    };
+
+    return {
+        init: function () {
+            $cubeBox.css('display', 'block');
+            //=>事件绑定实现相关效果
+            $box.attr({
+                rotateX: -30,
+                rotateY: 45
+            }).on({
+                touchstart: touchBegin,
+                touchmove: touching,
+                touchend: touchEnd
+            });
+
+            //=>每一个页面的点击操作
+            $box.find('li').tap(function () {
+                $cubeBox.css('display', 'none');
+                let index = $(this).index();
+                detailRender.init(index);
+            });
+        }
+    }
+})(Zepto);
 
 /*--DETAIL--*/
 let detailRender = (function ($) {
@@ -327,4 +326,4 @@ let detailRender = (function ($) {
     }
 })(Zepto);
 
-detailRender.init();
+cubeRender.init();
